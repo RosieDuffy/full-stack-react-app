0637ab53e5b00a45ed17276313a1d7764cbff91a
Rosie Duffy <rose.j.duffy@gmail.com>
2024-01-05 11:18:51 +0000

    Api url adjusted

--- client/src/utils/apiHelper.js
+++ client/src/utils/apiHelper.js
@@ -1,7 +1,7 @@
 // Function to make fetch requests to the api
 
 export const api = (path, method = "GET", body = null, credentials = null) => {
-  const url = "full-stack-react-app-production.up.railway.app" + path;
+  const url = "https://full-stack-react-app-production.up.railway.app/" + path;
 
   const options = {
     method,
